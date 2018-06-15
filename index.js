d8e0fd3ae77f9ec0aa958966198ce0b0331413d3
Masahide Isaka <a1132666@gmail.com>
2018-06-15 09:38:27 +0900

    Fix: indent 4 -> 2

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-let targetDirPath = 'src/' + process.argv[2] + '/',
-	bs = require('browser-sync').create('reveal-template');
+/**
+ *
+ * index
+ *
+ */
+
+'use strict';
+
+const bs = require('browser-sync').create('reveal-template');
+let targetDirPath = 'src/' + process.argv[2] + '/';
 
 bs.init({
-    server: true,
-	startPath: targetDirPath,
+  server: true,
+  startPath: targetDirPath,
 });
 
 bs.watch(targetDirPath + '*.html').on('change', bs.reload);
